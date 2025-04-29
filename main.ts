030a77326a83256a74c7ffe87c408c2606bf5d73
Denden Triana <denden.triana@gmail.com>
2025-04-29 22:14:11 +0700

    added some example usage

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { HijriScript } from "./hijriscript.ts";
+
+const todayHijriDate = HijriScript.today();
+console.log(`Hijri Date Today: ${todayHijriDate.toString()}`);
+
+const hijriDate = HijriScript.toHijri("29/04/2025");
+console.log(`Hijri Date: ${hijriDate.toString()}`);
+console.log(`Hijri Date custom format: ${hijriDate.toFormat("d-mm-YYN")}`);
+
+const gregorianDate = HijriScript.toGregorian("01/11/1446");
+console.log(`Gregorian Date: ${gregorianDate.toISOString()}`);
